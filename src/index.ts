e00053aa1a01d1b3d2dd3a929e0421b0bf622775
udhaya k <udhayak4251office@gmail.com>
2023-07-05 20:39:45 +0530

    Major changes completed and more parameterized

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
-import { CucumberJsonType } from "./interface/cucumber-json.interface";
+import { CucumberJsonType, Element } from "./interface/cucumber-json.interface";
 import { convertToJSON, isDirectoryExists, makeDirectory, readFile } from "./utils";
 import { utils, writeFile } from "xlsx";
+import * as _ from "lodash";
+
+type additionalPropsObject = {
+  columnName: string,
+  eval: string
+}
 
 type GenerateExcelReportProps = {
   cucumberJsonPaths: string[];
   cucumberExcelReportOutDir: string;
   fileName: string,
   includeSteps?: boolean,
-  includeLogs?:boolean,
-  includeJsonPath?:boolean
+  includeLogs?: boolean,
+  includeJsonPath?: boolean,
+  additionalProps?: additionalPropsObject[]
 };
 
 enum ResultEnum {
@@ -20,21 +27,20 @@ enum ResultEnum {
   ambiguous = "ambiguous",
 }
 
-const getTotalFailedCount = (uniqueId: string, rows: any[]) => {
-  return rows
-    .filter((row) => row.internalTrackId == uniqueId)
-    .filter((row) => row.status !== ResultEnum.passed)?.length;
-};
-
-const getCountOfStatus = (
-  uniqueId: string,
-  rows: any[],
-  status: ResultEnum
-) => {
-  return rows
-    .filter((row) => row.internalTrackId == uniqueId)
-    .filter((row) => row.status == status)?.length;
-};
+const getStatus = (element: Element) => {
+  const passedCount = element.steps.filter(step => step.result.status == ResultEnum.passed).length;
+  const failedCount = element.steps.filter(step => step.result.status == ResultEnum.failed).length;
+  const undefinedCount = element.steps.filter(step => step.result.status == ResultEnum.undefined).length;
+  if (passedCount == element.steps.length) {
+    return ResultEnum.passed
+  } else if (failedCount > 0) {
+    return ResultEnum.failed
+  } else if (undefinedCount > 0) {
+    return ResultEnum.undefined
+  } else {
+    return 'unknown'
+  }
+}
 
 const GenerateExcelReport = ({
   cucumberJsonPaths,
@@ -42,155 +48,124 @@ const GenerateExcelReport = ({
   fileName,
   includeSteps,
   includeLogs,
-  includeJsonPath
+  includeJsonPath,
+  additionalProps
 }: GenerateExcelReportProps) => {
-  let allRows = [];
+  let allRows: any = [];
+  let additionalColumnNames:string[] = [];
+  //adding additional columns for scenario and this step is not needed for steps since one time activity is enough and scenario data will be processed in all case
+  additionalProps?.forEach((prop)=> {
+    additionalColumnNames.push(prop.columnName);
+  });
+
   for (let i = 0; i < cucumberJsonPaths.length; i++) {
     const cucumberJsonPath = cucumberJsonPaths[i];
     //read data
     const rawFileContent = readFile(cucumberJsonPath);
     //Convert into JSON
     const jsonCucumberData: CucumberJsonType = convertToJSON(rawFileContent);
-    let rows: any[] = [];
     //read data from Json
     jsonCucumberData.forEach((report) => {
       report.elements.forEach((element) => {
-        element.steps.forEach((step) => {
-          rows.push({
-            internalTrackId: `${report.id}_${element.id}`,
-            hooks: step.keyword,
-            tags: element.tags.map((tag) => tag.name)?.toString(),
-            featureId: report.id,
-            feature: report.name,
-            scenarioName: element.name,
-            stepName: `${step.keyword}${step.name}`,
-            id: element.id,
-            status: step.result.status,
-            logs: step.result.error_message
-          });
+        //addition props adding process
+        let tempProps:any = {};
+        additionalProps?.forEach((prop)=> {
+          tempProps[prop.columnName] = eval(prop.eval);
+        })
+        //Scenario Level data processing
+        allRows.push({
+          internalTrackId: `${report.id}_${element.id}`,
+          hooks: "",
+          tags: element.tags.map((tag) => tag.name.replace('@', ''))?.toString(),
+          featureId: report.id,
+          feature: report.name,
+          scenarioName: element.name,
+          stepName: "",
+          id: element.id,
+          status: getStatus(element),
+          logs: "",
+          cucumberJsonPath,
+          ...tempProps
         });
+        //process steps only if includeSteps is true
+        if (includeSteps == true) {
+        //addition props adding process
+        let tempProps:any = {};
+        additionalProps?.forEach((prop)=> {
+          tempProps[prop.columnName] = eval(prop.eval);
+        })
+          //Step Level data processing
+          element.steps.forEach((step, index) => {
+            if (!(step.keyword.includes('Before') || step.keyword.includes('After'))) {
+              allRows.push({
+                internalTrackId: `${report.id}_${element.id}`,
+                hooks: step.keyword,
+                tags: "",
+                featureId: "",
+                feature: "",
+                scenarioName: "",
+                stepName: `${step.keyword}${step.name}`,
+                id: "",
+                status: step.result.status,
+                logs: step.result.error_message,
+                cucumberJsonPath
+              });
+            }
+          });
+        }
       });
     });
-    
-    //find unique ids
-    const findUniqueScenarioIds = Array.from(new Set(
-      rows.map((data) => data.internalTrackId)
-    ));
-
-    let scenarioWiseRows: any[] = [];
-
-    findUniqueScenarioIds.forEach((uniqueId: string, index: number) => {
-      const failedCount = getTotalFailedCount(uniqueId, rows);
-      const failed = getCountOfStatus(uniqueId, rows, ResultEnum.failed);
-      const undefined = getCountOfStatus(uniqueId, rows, ResultEnum.undefined);
-      if (includeSteps) {
-        let stepsFormedData: any[] = [];
-        //add scenario data into stepsFormedData
-        let scenario = rows.find((scenario) => scenario.internalTrackId == uniqueId);
-        if (failedCount == 0) {
-          scenario.status = ResultEnum.passed;
-        } else if (failed > 0) {
-          scenario.status = ResultEnum.failed;
-        } else if (undefined > 0) {
-          scenario.status = ResultEnum.undefined;
-        } else {
-          scenario.status = "unknown";
-        }
-        const { featureId, id, internalTrackId, ...filterScenario } = scenario;
-        let filteredScenario:any = {};
-        if(includeLogs) {
-          filteredScenario = {
-            sNo: index + 1,
-            tags:filterScenario.tags,
-            feature: filterScenario.feature,
-            scenarioName: filterScenario.scenarioName,
-            stepName: '',
-            status: filterScenario.status,
-            Logs: ''
-          }
-        } else {
-          filteredScenario ={
-            sNo: index + 1,
-            tags:filterScenario.tags,
-            feature: filterScenario.feature,
-            scenarioName: filterScenario.scenarioName,
-            stepName: '',
-            status: filterScenario.status
-          }
-        }
-
-        if(includeJsonPath) {
-          filteredScenario.Source = cucumberJsonPath
-        }
-        
-        stepsFormedData.push(filteredScenario);
-        //add steps data into stepsFormedData
-        let scenarioSteps = rows.filter((scenario) => scenario.internalTrackId == uniqueId && !scenario.hooks.includes('Before'));
-        scenarioSteps.forEach(step => {
-          const { internalTrackId, featureId, id, feature, scenarioName, tags, ...rest } = step;
-         
-          let onlyStep:any = {};
-          if(includeLogs) {
-              onlyStep = {
-                sNo: '',
-                feature: '',
-                scenarioName: '',
-                tags: '',
-                stepName: rest.stepName,
-                status: rest.status,
-                Logs: rest.logs,
-              }
-          } else {
-            onlyStep = {
-              sNo: '',
-              feature: '',
-              scenarioName: '',
-              tags: '',
-              stepName: rest.stepName,
-              status: rest.status
-            }
-          }
-          if(includeJsonPath) {
-            onlyStep.Source = cucumberJsonPath
-          }
-          stepsFormedData.push(onlyStep);
-        })
-        scenarioWiseRows.push(...stepsFormedData);
-        
-
-      } else {
-        let scenario = rows.find((scenario) => scenario.internalTrackId == uniqueId);
-        if (failedCount == 0) {
-          scenario.status = ResultEnum.passed;
-        } else if (failed > 0) {
-          scenario.status = ResultEnum.failed;
-        } else if (undefined > 0) {
-          scenario.status = ResultEnum.undefined;
-        } else {
-          scenario.status = "unknown";
-        }
-
-        const { internalTrackId, featureId, id, stepName, hooks,  ...rest } = scenario;
-        const scenarioData = { sno: scenarioWiseRows.length + 1, ...rest };
-        scenarioWiseRows.push(scenarioData);
-      }
-
-    });
-
-    //add set of scenario status to all scenarios set
-    allRows.push(...scenarioWiseRows);
   }
 
+  
+  //default column names for scenario
+  let defaultColumnNamesForScenario = ["sNo", "tags", "feature", "scenarioName", "status", ...additionalColumnNames];
+  let defaultColumnNamesForSteps = ["sNo", "tags", "feature", "scenarioName", "stepName", "status", ...additionalColumnNames];
+
+  
+  if (includeLogs == true) {
+    defaultColumnNamesForSteps.push("logs")
+  }
+
+  if (includeJsonPath == true) {
+    defaultColumnNamesForScenario.push("cucumberJsonPath")
+    defaultColumnNamesForSteps.push("cucumberJsonPath")
+  }
+
+  let filteredRows: any[] = [];
+  //adding serial number to the rows at scenario level
+  if (includeSteps == false) {
+    filteredRows = allRows.map((row: any, index: number) => {
+      const pickedObject = _.pick(row, defaultColumnNamesForScenario);
+      const testCaseNumber = index + 1;
+      return { sNo: testCaseNumber, ...pickedObject }
+    })
+  }
+  //adding serial number to the rows at step level
+  if (includeSteps == true) {
+    filteredRows = allRows.map((row: any, index: number) => {
+      const pickedObject = _.pick(row, defaultColumnNamesForSteps);
+      const testCaseNumber = index + 1;
+      return { sNo: testCaseNumber, ...pickedObject }
+    })
+  }
+
+
+
   /* generate worksheet and workbook */
-  const worksheet = utils.json_to_sheet(allRows);
+  const changeFirstLetterToUppercase = (columnNames: string[]) => {
+    return columnNames.map(e => e.substring(0, 1).toUpperCase() + e.substring(1))
+  }
+
+  const worksheet = utils.json_to_sheet(filteredRows);
   const workbook = utils.book_new();
   utils.book_append_sheet(workbook, worksheet, "Cucumber Excel Report");
   if (includeSteps) {
     /* fix headers */
-    utils.sheet_add_aoa(worksheet, [["S.No", "Tags", "Feature", "Scenario", "Step", "Status"]], { origin: "A1" });
+    utils.sheet_add_aoa(worksheet, [changeFirstLetterToUppercase(defaultColumnNamesForSteps)], { origin: "A1" });
   } else {
     /* fix headers */
-    utils.sheet_add_aoa(worksheet, [["S.No", "Tags", "Feature", "Scenario", "Status"]], { origin: "A1" });
+    utils.sheet_add_aoa(worksheet, [changeFirstLetterToUppercase(defaultColumnNamesForScenario)], { origin: "A1" });
   }
 
 
--- test/e2e/test.ts
+++ test/e2e/test.ts
@@ -5,6 +5,9 @@ GenerateExcelReport({
     cucumberExcelReportOutDir: "cucumber_excel_report",
     fileName: 'Cucumber_Excel_report',
     includeSteps:true,
-    includeLogs: true,
-    includeJsonPath: true
+    includeLogs: false,
+    includeJsonPath: false,
+    additionalProps: [
+        {columnName: "Test Case Id", eval: "element.tags.map((tag) => tag.name)?.toString().match(/\\d{6}/g)?.toString()"}
+    ]
 })
